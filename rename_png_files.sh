e0abc8792362ef81327b5e1dbe229cac1c8b3c22
Cyril Soler <cyril.soler@inria.fr>
2021-08-09 12:22:05 +0200

    added script to automatically convert/rename png images

---
+++ rename_png_files.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo This script converts and renames all png files in the current directory into jpg files as properly numbered file_???.jpg
+echo Default does nothing. Use -f to actually do the job.
+
+currentnum="0"
+files=`ls *.png`
+
+find . -name "*.png" -print | while read file
+do 
+	padded=`seq -f "%03g" $currentnum $currentnum`
+	newname=file_"$padded".jpg
+
+	echo padded is '"'$padded'"'
+	echo file is '"'$file'"'
+	echo new name is '"'$newname'"'
+
+	if test "$1" = "-f" ; then
+		convert "$file" $newname
+	else
+		echo convert \"$file\" $newname
+	fi
+
+	currentnum=`expr $currentnum + 1`
+done
